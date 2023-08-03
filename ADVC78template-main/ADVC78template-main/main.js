1b3d8bb1f5037c5592cc7226788a01c5a4e3d9d3
s0hanp51412 <141365335+s0hanp51412@users.noreply.github.com>
2023-08-03 15:10:26 -0500

    Add files via upload

---
+++ ADVC78template-main/ADVC78template-main/main.js
@@ -0,0 +1,20 @@
+var images = ["img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg"];
+var names = ["Mom", "Dad", "Riya", "Bani", "Sohan"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 5
+    if(i > numbers_of_family_member_in_array)
+      {
+          i = 0;
+      }
+    
+    //Debug the code to store list of images in updatedImage. Use images[i]
+    var updatedImage = images[i];
+    //Debug the code to store list of names in updatedName. Use names[i]
+    var updatedName = names[i] ;
+ 
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
